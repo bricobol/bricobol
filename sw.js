86f15cc0541fa0a6caaf0ea6bab0138fd000e695
bricobol <bricobol.asso@proton.me>
2026-09-22 19:32:43 +0200

    Fusion Session 4a: bouton Frais dans Missions + enrichissement prorata

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Cache l'app pour hors-ligne + mises à jour auto
 // ============================================================
 
-const CACHE = 'bricobol-v14';
+const CACHE = 'bricobol-v16';
 const ASSETS = ['/', '/bricobol/', '/bricobol/index.html', '/bricobol/manifest.json', '/bricobol/icon.svg'];
 
 self.addEventListener('install', (e) => {
